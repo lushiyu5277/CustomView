23669fabbfd400b5a5a3ec42d9a5220aeb5d2c9d
shiyulu <shiyulu@tencent.com>
2018-07-23 19:01:42 +0800

    Add MaterialEditText

--- app/src/main/java/com/geek/shiyulu/customview/canvasview/SportView.java
+++ app/src/main/java/com/geek/shiyulu/customview/canvasview/SportView.java
@@ -2,6 +2,9 @@ package com.geek.shiyulu.customview.canvasview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -15,9 +18,10 @@ import com.geek.shiyulu.customview.utils.UIUtil;
 
 public class SportView extends View {
 
-    final private static float RADIUS = UIUtil.dpToPixel(100);
+    final private static float RADIUS = UIUtil.dpToPixel(150);
 
     private RectF rectF = new RectF();
+    private Paint paint = new Paint();
 
     public SportView(Context context) {
         super(context);
@@ -35,6 +39,20 @@ public class SportView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         rectF.set(getWidth() / 2 - RADIUS, getHeight() / 2 - RADIUS, getWidth() / 2 + RADIUS, getHeight() / 2 + RADIUS);
-        
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.DKGRAY);
+        paint.setStrokeWidth(UIUtil.dpToPixel(10));
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, RADIUS, paint);
+        paint.setColor(Color.RED);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        canvas.drawArc(rectF, -120, 300, false, paint);
+        String drawText = "SPort";
+        paint.setTextSize(UIUtil.dpToPixel(70));
+        paint.setStyle(Paint.Style.FILL);
+        paint.setTextAlign(Paint.Align.CENTER);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        Rect bounds = new Rect();
+        paint.getTextBounds(drawText, 0, drawText.length(), bounds);
+        canvas.drawText(drawText, getWidth() / 2, getHeight() / 2 + (bounds.bottom - bounds.top) / 2, paint);
     }
 }
---
+++ app/src/main/java/com/geek/shiyulu/customview/extendview/MaterialEditText.java
@@ -0,0 +1,36 @@
+package com.geek.shiyulu.customview.extendview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+
+import com.geek.shiyulu.customview.utils.UIUtil;
+
+public class MaterialEditText extends android.support.v7.widget.AppCompatEditText {
+
+    private static final int HEADER_OFFSET = (int)UIUtil.dpToPixel(20);
+
+    private CharSequence mFloatingText;
+
+    public MaterialEditText(Context context) {
+        super(context);
+    }
+
+    public MaterialEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mFloatingText = 
+        if (getHint() != null && getHint().length() > 0) {
+            setPadding(getPaddingLeft(), getPaddingTop() + HEADER_OFFSET, getPaddingRight(), getPaddingBottom());
+        }
+    }
+
+    public MaterialEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+}

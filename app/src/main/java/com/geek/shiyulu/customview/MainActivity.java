477e48013f4e1e2e95ef18e227e8f98a20d74985
shiyulu <shiyu0898@gmail.com>
2019-01-07 21:29:00 +0800

    add viewpager

--- app/src/main/java/com/geek/shiyulu/customview/MainActivity.java
+++ app/src/main/java/com/geek/shiyulu/customview/MainActivity.java
@@ -13,12 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        logoView = findViewById(R.id.zhihuView);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-//        logoView.startAnimator();
     }
 }
---
+++ app/src/main/java/com/geek/shiyulu/customview/adapter/ViewFragmentPagerAdapter.java
@@ -0,0 +1,22 @@
+package com.geek.shiyulu.customview.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class ViewFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
